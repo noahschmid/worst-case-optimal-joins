17ed544d9c89e74987c1f1e72f3b4352d844d347
ctanir <ctanir@student.ethz.ch>
2023-05-01 00:08:58 +0200

    Adapt benchmarking for new Table

--- benchmark/algo_2_benchmark.cpp
+++ benchmark/algo_2_benchmark.cpp
@@ -13,6 +13,7 @@
 #include "../tuple_list.h"
 #include "../hash_trie.h"
 #include "../query.h"
+#include "../database.h"
 using namespace std;
 
 //#define CALIBRATE
@@ -57,39 +58,56 @@ double rdtsc(Table* table, std::vector<std::string> attributes) {
 
 
 int main(int argc, char **argv) {
-    Table* table;
-    std::vector<std::string> attributes;
-    if (argc == 3){
-        int num_columns = atoi(argv[1]);
-        int num_rows = atoi(argv[2]);
-        printf("columns=%d, rows=%d\n", num_columns, num_rows);
 
-        for (int i=0;i<num_columns;i++){
-            attributes.push_back("v");
-        }
+    std::string path_to_tsv;
+    std::string table_name;
+
+    // incorrect number of arguments
+    if (argc != 3){
+        printf("Usage: <path_to_tsv> <table_name>\n");
+        return -1;
+    }else{ 
+        //get the strings from the command line
+        path_to_tsv = argv[1];
+        table_name = argv[2];
+
+    }
+
+    Table* table = new Table(path_to_tsv, table_name);
+
+    // std::vector<std::string> attributes;
+    // if (argc == 3){
+    //     int num_columns = atoi(argv[1]);
+    //     int num_rows = atoi(argv[2]);
+    //     printf("columns=%d, rows=%d\n", num_columns, num_rows);
+
+    //     for (int i=0;i<num_columns;i++){
+    //         attributes.push_back("v");
+    //     }
         
 
-        table = new Table(num_columns);
+    //     table = new Table(num_columns);
 
-        for(int i=0;i<num_rows;i++){
-            std::vector<int> vec;
-            for (int i=0;i<num_columns;i++){
-                vec.push_back(i);
-            }
-            table->append_row(vec);
-        }
-    }
-    else if(argc == 2){
-        table = new Table(argv[1]);
-        attributes = table->get_attributes();
-        printf("%d\n", table->get_num_rows());
-    }
-    else{printf("usage: <num_columns> <num_rows> OR <path_to_tsv>\n"); return -1;}
-    
+    //     for(int i=0;i<num_rows;i++){
+    //         std::vector<int> vec;
+    //         for (int i=0;i<num_columns;i++){
+    //             vec.push_back(i);
+    //         }
+    //         table->append_row(vec);
+    //     }
+    // }
+    // else if(argc == 2){
+    //     table = new Table(argv[1]);
+    //     attributes = table->get_attributes();
+    //     printf("%d\n", table->get_num_rows());
+    // }
+    // else{printf("usage: <num_columns> <num_rows> OR <path_to_tsv>\n"); return -1;}
 
-    printf("starting benchmark...\n");
-    double r = rdtsc(table, attributes);
-    printf("benchmark finished\n");
+
+
+    printf("Starting benchmark...\n");
+    double r = rdtsc(table, table->get_attributes());
+    printf("Benchmark finished!\n");
     printf("RDTSC instruction:\n %lf cycles measured => %lf seconds, assuming frequency is %lf MHz. (change in source file if different)\n\n", r, r/(FREQUENCY), (FREQUENCY)/1e6);
     return 0;
 }
