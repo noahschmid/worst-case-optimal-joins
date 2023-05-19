66d058c757e7d11b40aeefa837d999d2b5e44b69
Alexander De Battista Kvamme <alexanderdebattistakvamme@gmail.com>
2023-05-19 16:50:14 +0200

    Updated benchmark for alg3 to accommodate new interface, and made implenentation more efficient.
    Former-commit-id: 3e938abfba78b0c9d1e365e9e087348fcdce6b9a

--- benchmark/algo_3_benchmark.cpp
+++ benchmark/algo_3_benchmark.cpp
@@ -15,13 +15,58 @@
 #include "../query.h"
 using namespace std;
 
-#define CALIBRATE
+//#define CALIBRATE
 #define NUM_RUNS 5
-#define CYCLES_REQUIRED 1e10
+#define CYCLES_REQUIRED 1e7
 #define FREQUENCY 2.8e9
 
-double rdtsc(JoinQuery* query) {
+double rdtsc(std::vector<std::string> tables_str, int num_tables, std::vector<std::string> attributes) {
     int i, num_runs;
+    myInt64 cycles;
+    myInt64 start;
+    num_runs = NUM_RUNS;
+    const Table* tables[num_tables];
+    Table* tbl;
+    for(int i=0;i<num_tables;i++){
+            tables[i] = new Table(tables_str.at(i), tables_str.at(i));
+    }
+
+    /* 
+     * The CPUID instruction serializes the pipeline.
+     * Using it, we can create execution barriers around the code we want to time.
+     * The calibrate section is used to make the computation large enough so as to 
+     * avoid measurements bias due to the timing overhead.
+     */
+#ifdef CALIBRATE
+    while(num_runs < (1 << 14)) {
+        start = start_tsc();
+        for (i = 0; i < num_runs; ++i) {
+            for(int i=0;i<num_tables;i++){
+                tables[i] = new Table(tables_str.at(i), tables_str.at(i));
+            }
+            tbl = JoinQuery(tables, num_tables, attributes).exec();
+        }
+        cycles = stop_tsc(start);
+
+        if(cycles >= CYCLES_REQUIRED) break;
+
+        num_runs *= 2;
+    }
+#endif
+    
+    start = start_tsc();
+    for (i = 0; i < num_runs; ++i) {
+        JoinQuery q(tables, num_tables, attributes);
+        tbl = q.exec();
+        std::cout << *tbl << std::endl;
+    }
+
+    cycles = stop_tsc(start)/num_runs;
+    return (double) cycles;
+}
+
+double c_clock(const Table** tables, int num_tables, std::vector<std::string> attributes) {
+        int i, num_runs;
     myInt64 cycles;
     myInt64 start;
     num_runs = NUM_RUNS;
@@ -37,7 +82,10 @@ double rdtsc(JoinQuery* query) {
     while(num_runs < (1 << 14)) {
         start = start_tsc();
         for (i = 0; i < num_runs; ++i) {
-            tbl = query->exec();
+            for(int i=0;i<num_tables;i++){
+                tables[i] = new Table(tables_str.at(i), tables_str.at(i));
+            }
+            tbl = JoinQuery(tables, num_tables, attributes).exec();
         }
         cycles = stop_tsc(start);
 
@@ -46,46 +94,16 @@ double rdtsc(JoinQuery* query) {
         num_runs *= 2;
     }
 #endif
-
-    start = start_tsc();
-    for (i = 0; i < num_runs; ++i) {
-        tbl = query->exec();
-    }
-
-    cycles = stop_tsc(start)/num_runs;
-    return (double) cycles;
-}
-
-double c_clock(JoinQuery* query) {
-    int i, num_runs;
-    double cycles;
-    clock_t start, end;
-    Table* tbl;
-
-    num_runs = NUM_RUNS;
-#ifdef CALIBRATE
-    while(num_runs < (1 << 14)) {
-        start = clock();
-        for (i = 0; i < num_runs; ++i) {
-            tbl = query->exec();
-        }
-        end = clock();
-
-        cycles = (double)(end-start);
-
-        // Same as in c_clock: CYCLES_REQUIRED should be expressed accordingly to the order of magnitude of CLOCKS_PER_SEC
-        if(cycles >= CYCLES_REQUIRED/(FREQUENCY/CLOCKS_PER_SEC)) break;
-
-        num_runs *= 2;
-    }
-#endif
+    
     start = clock();
-    for(i=0; i<num_runs; ++i) { 
-        tbl = query->exec();
+    for (i = 0; i < num_runs; ++i) {
+        JoinQuery q(tables, num_tables, attributes);
+        tbl = q.exec();
+        //std::cout << *tbl << std::endl;
     }
-    end = clock();
 
-    return (double)(end-start)/num_runs;
+    int stop = clock();
+    return (double) (stop-start)/num_runs;
 }
 
 
@@ -94,24 +112,25 @@ int main(int argc, char **argv) {
         printf("usage: <NUM_TABLES> <PATH_TO_TABLE_1> ... <PATH_TO_TABLE_n> <COL_TO_JOIN_1> ... <COL_TO_JOIN_N>\n"); return -1;
     }
     int num_tables = atoi(argv[1]);
-    Table* tables[argc-1];
+    const Table* tables[num_tables];
+    std::vector<std::string> table_path;
     std::vector<std::string> attributes;
-    for(int i=2;i<num_tables+2;i++){
-        tables[i-2] = new Table(argv[i], argv[i]);
-    }
 
     for(int i=num_tables+2;i<argc;i++){
         attributes.push_back(argv[i]);
     }
-    
-    JoinQuery query(tables, num_tables, attributes);
+
+    for(int i=2;i<num_tables+2;i++){
+        tables[i-2] = new Table(argv[i], argv[i]);
+        table_path.push_back(argv[i]);
+    }
 
 
     printf("starting benchmark...\n");
-    double r = rdtsc(&query);
-    double c = c_clock(&query);
+    //double r = rdtsc(table_path, num_tables, attributes);
+    double c = c_clock(tables, num_tables, attributes);
     printf("benchmark finished\n");
-    printf("RDTSC instruction:\n %lf cycles measured => %lf seconds, assuming frequency is %lf MHz. (change in source file if different)\n\n", r, r/(FREQUENCY), (FREQUENCY)/1e6);
+    //printf("RDTSC instruction:\n %lf cycles measured => %lf seconds, assuming frequency is %lf MHz. (change in source file if different)\n\n", r, r/(FREQUENCY), (FREQUENCY)/1e6);
     printf("C clock() function:\n %lf cycles measured. On some systems, this number seems to be actually computed from a timer in seconds then transformed into clock ticks using the variable CLOCKS_PER_SEC. Unfortunately, it appears that CLOCKS_PER_SEC is sometimes set improperly. (According to this variable, your computer should be running at %lf MHz). In any case, dividing by this value should give a correct timing: %lf seconds. \n\n",c, (double) CLOCKS_PER_SEC/1e6, c/CLOCKS_PER_SEC);
     return 0;
 }
