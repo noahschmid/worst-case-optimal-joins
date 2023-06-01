05a23b6bb1aab50004a2732c46ea7abc712c42be
Xianzhe Ma <xianzma@gmail.com>
2023-06-01 16:02:18 +0200

    add warm-up iteration and make algo2,3 against mini dataset
    Former-commit-id: aff874f708b6695a7b3ea0ea069c262d53cd0f66

--- benchmark/algo_2_measure.py
+++ benchmark/algo_2_measure.py
@@ -1,5 +1,15 @@
 import os
 
+def parse_output(output: str):
+    # return the number of cycles and the duration
+    num_cycles = int(output.split("RDTSC instruction:")[1].split(" ")[1].split(".")[0])
+    duration = float(output.split("timing:")[1].split(" ")[1])
+    return num_cycles, duration
+
+def run(cmd: str):
+    # run the command and return the output
+    return os.popen(cmd).read()
+
 def run_program(name: str, num_cols: int):
     list_num_rows = []
     list_num_cycles = []
@@ -9,12 +19,11 @@ def run_program(name: str, num_cols: int):
         # call the program with the given number of columns and rows
         # and get the output, use windows and linux compatible path
         path = os.path.join(os.getcwd(), name)
-        output = os.popen(f"{path} {num_cols} {i}").read()
+        output = run(f"{path} {num_cols} {i}")
         #output = os.popen(f".\\2\{name} {num_cols} {i}").read()
 
 
-        num_cycles = int(output.split("RDTSC instruction:")[1].split(" ")[1].split(".")[0])
-        duration = float(output.split("timing:")[1].split(" ")[1])
+        num_cycles, duration = parse_output(output)
 
         list_num_rows.append(i)
         list_num_cycles.append(num_cycles)
@@ -24,11 +33,34 @@ def run_program(name: str, num_cols: int):
     print(list_num_cycles)
     print(list_duration)
 
+def run_against_mini_data():
+    table_paths = [
+        './data/mini_mini_data_1.tsv',
+        './data/mini_mini_data_2.tsv',
+        './data/mini_mini_data_3.tsv',
+    ]
+
+    list_num_cycles = []
+    list_duration = []
+    for table_path in table_paths:
+        cmd = f"./bm2.exe {table_path}"
+        output = run(cmd)
+        cycles, dur = parse_output(output)
+        list_num_cycles.append(cycles)
+        list_duration.append(dur)
+
+    print(list_num_cycles)
+    print(list_duration)
+
+
 def main():
 
     # run the program for 2 to 6 columns
-    for i in range(2, 7):
-        run_program("../bm2.exe", i)
+    # for i in range(2, 7):
+    #     run_program("../bm2.exe", i)
+    run_against_mini_data()
+
+
 
 
 if __name__ == "__main__":
