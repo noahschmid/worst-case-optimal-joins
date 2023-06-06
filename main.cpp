48296846c66be6ce8d41c1cb2de1188b88848723
ctanir <ctanir@student.ethz.ch>
2023-06-06 14:19:05 +0200

    use mini mini set
    Former-commit-id: 63fc80db7cbf42b9d3a962b764d8fe17b191dab9

--- main.cpp
+++ main.cpp
@@ -7,16 +7,16 @@ using namespace std;
 int main(int argc, char **argv) {
     // Initialize database
     Database db = Database();
-    db.load_table("./data/r1.tsv", "R1");
-    db.load_table("./data/r2.tsv", "R2");
-    db.load_table("./data/r3.tsv", "R3");
+    db.load_table("./data/mini_mini_data_1.tsv", "R1");
+    db.load_table("./data/mini_mini_data_2.tsv", "R2");
+    db.load_table("./data/mini_mini_data_3.tsv", "R3");
 
     // Executes algorithms 2 & 3 and retrieves join query results
     Table *result = db.query("JOIN R1,R2,R3 ON v1,v2,v3");
 
     // Print results
-    if(result)
-        std::cout << *result << std::endl;
+    //if(result)
+        //std::cout << *result << std::endl;
 
     delete result;
 }
