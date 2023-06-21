095645db72967986518b833e4f8b1c08a61d2868
Alexander De Battista Kvamme <alexanderdebattistakvamme@gmail.com>
2023-06-21 15:45:01 +0200

    Implemented way to calculate joins per second
    Former-commit-id: 9f0a888e463811e9db5ffb42435fa63ceb0c04c8

---
+++ data/throughput_val.py
@@ -0,0 +1,31 @@
+import os
+cycles = []
+std_dev = []
+duration = []
+size = []
+for i in range(22, 40):
+    os.system(f"python3.10 data/create_datasets.py {i}")
+    res = os.popen(f"python3.10 benchmark/e2e_measure.py").read()
+
+    f = open("data/int_res.txt", "w")
+    cycles.append(res.split("cycles:")[1].split(" ")[1].split("\n")[0])
+    duration.append(res.split("duration:")[1].split(" ")[1].split("\n")[0])
+    std_dev.append(res.split("stddev:")[1].split(" ")[1].split("\n")[0])
+    size.append(i)
+    f.write("cycles: ")
+    f.write(str(cycles))
+    f.write("\n")
+    f.write("duration: ")
+    f.write(str(duration))
+    f.write("\n")
+    f.write("stddev: ")
+    f.write(str(std_dev))
+    f.write("\n")
+    f.write("size: ")
+    f.write(str(size))
+    f.write("\n")
+    f.close()
+    print(i)
+print(cycles)
+print(std_dev)
+print(duration)
