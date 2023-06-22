a73e2c47d0f0a94d4727b56c0ac24f0fee7201fc
Alexander De Battista Kvamme <alexanderdebattistakvamme@gmail.com>
2023-06-22 09:39:24 +0200

    Fixed parsing
    Former-commit-id: adb6f386772534e019dad2c36ed2268ee19a37ca

--- data/throughput_val.py
+++ data/throughput_val.py
@@ -3,14 +3,14 @@ cycles = []
 std_dev = []
 duration = []
 size = []
-for i in range(1, 40):
+for i in range(27, 40):
     os.system(f"python3.10 data/create_datasets.py {i}")
     res = os.popen(f"python3.10 benchmark/e2e_measure.py").read()
 
     f = open("data/int_res.txt", "w")
-    cycles.append(res.split("cycles:")[1].split(" ")[1])
-    duration.append(res.split("duration:")[1].split(" ")[1])
-    std_dev.append(res.split("stddev:")[1].split(" ")[1])
+    cycles.append(float(res.split("cycles:")[1].split(" ")[1].split("\n")[0]))
+    duration.append(float(res.split("duration:")[1].split(" ")[1].split("\n")[0]))
+    std_dev.append(float(res.split("stddev:")[1].split(" ")[1].split("\n")[0]))
     size.append(i)
     f.write("cycles: ")
     f.write(str(cycles))
